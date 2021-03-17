03a1bbc6346bcbb36bc3108fc9433d07fca06ca1
王隔壁 <61082747+1276303594@users.noreply.github.com>
2021-03-17 12:56:40 +0800

    2021/3/17

--- remove_all_blank_folder.py
+++ remove_all_blank_folder.py
@@ -8,11 +8,12 @@ cwd = os.path.abspath('.')      # 获取当前目录的绝对路径
 
 for i in dirs:
     dirs2 = os.walk(i)    # 获取文件夹下所有的子目录
+    dirs2 = list(dirs2)[::-1]    # 倒叙
     for j in dirs2:
-        dir3 = os.path.join(cwd, j[0])    # 获取子目录的绝对路径
+        dirs3 = os.path.join(cwd, j[0])    # 获取子目录的绝对路径
         try:
-            os.removedirs(dir3)    # 删除空白子目录
-            print('删除空文件夹{}'.format(dir3))
+            os.removedirs(dirs3)    # 删除空白子目录
+            print('删除空文件夹{}'.format(dirs3))
             nums += 1
         except OSError:
             pass
